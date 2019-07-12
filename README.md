82c4e9d77108788a36ae3e17d5fae57d5581690b
Patryk Sadok <patryk.sadok@gmail.com>
2019-07-12 12:20:18 +0200

    Initial structure

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Repository used to develop my JavaScript skills
 REST API architecture, with purpose to make CRUD operations on simple MySQL database.
 
 ## Used techs:
-
+* Express.js
+* Sequelize
+  
 ## Requirements to run:
 
 ## Commands:
@@ -14,3 +16,4 @@ REST API architecture, with purpose to make CRUD operations on simple MySQL data
 * **12.07.19 v.0.0.1**
   * *Added*
     * README.md
+    * Initial structure of project
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "OPTIONS", "PATCH", "DELETE"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization"
+    ],
+    credentials: true
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require("./routers/api"));
+
+app.listen(port, () => {
+  console.log(`Server is running live on port: ${port}`);
+});
