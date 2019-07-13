9b06ceaebd25e897ac5e15f5d9940c8b7e86812c
Patryk Sadok <patryk.sadok@gmail.com>
2019-07-13 16:43:29 +0200

    Updated tests

--- tests/repositories/vehicleRepository.test.js
+++ tests/repositories/vehicleRepository.test.js
@@ -1,7 +1,22 @@
 const assert = require("assert");
+const { expect } = require("chai");
+const proxyquire = require("proxyquire");
+// const Vehicle
+const VehicleRepository = proxyquire("../repositories/vehicleRepository");
+describe("Parking repository tests", () => {
+  let repository = null;
 
-describe("smoke test", function() {
-  it("checks equality", function() {
-    assert.equal(true, true);
+  beforeEach(() => {
+    repository = new VehicleRepository();
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe("Returns array of vehicles: ", () => {
+    it("checks equality", function() {
+      assert.equal(true, true);
+    });
   });
 });
